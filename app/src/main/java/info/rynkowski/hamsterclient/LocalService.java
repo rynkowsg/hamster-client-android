50166074d29ae9443ea88d339dd524b651fd1865
Grzegorz Rynkowski <grzegorz.rynkowski@gmail.com>
2014-12-07 18:08:21 +0100

    Added ongoing notification for service

--- app/src/main/java/info/rynkowski/hamsterclient/LocalService.java
+++ app/src/main/java/info/rynkowski/hamsterclient/LocalService.java
@@ -1,6 +1,11 @@
 package info.rynkowski.hamsterclient;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.app.TaskStackBuilder;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
@@ -22,6 +27,7 @@ public class LocalService extends Service {
     public void onCreate() {
         super.onCreate();
         Log.d(TAG, "onCreate()");
+        initNotification();
     }
 
     @Override
@@ -34,6 +40,7 @@ public class LocalService extends Service {
     @Override
     public void onDestroy() {
         Log.d(TAG, "onDestroy()");
+        cancelNotification();
         super.onDestroy();
     }
 
@@ -43,6 +50,30 @@ public class LocalService extends Service {
         return mBinder;
     }
 
+    // https://developer.android.com/guide/topics/ui/notifiers/notifications.html#SimpleNotification
+    private void initNotification() {
+        Intent clickNotification = new Intent(this, MainActivity.class);
+        PendingIntent resultPendingIntent = TaskStackBuilder.create(this)
+                .addParentStack(MainActivity.class)
+                .addNextIntent(clickNotification)
+                .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        Notification notification = new Notification.Builder(this)
+                .setSmallIcon(R.drawable.hamster_time_tracker)
+                .setContentTitle("Hamster Time Tracker")
+                .setContentText("It works!")
+                .setContentIntent(resultPendingIntent)
+                .setOngoing(true)
+                .build();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(NOTIFICATION_ID, notification);
+    }
+
+    private void cancelNotification() {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.cancel(NOTIFICATION_ID);
+    }
+
     private final String TAG = this.getClass().getSimpleName();
     private final IBinder mBinder = new LocalBinder();
+    private static final int NOTIFICATION_ID = 1;
 }
