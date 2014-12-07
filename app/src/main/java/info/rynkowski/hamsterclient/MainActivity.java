cd2834a394a0ff56bf16ade85bb04291c17ab507
Grzegorz Rynkowski <grzegorz.rynkowski@gmail.com>
2014-12-07 13:49:38 +0100

    Dodano przeladowane metody

--- app/src/main/java/info/rynkowski/hamsterclient/MainActivity.java
+++ app/src/main/java/info/rynkowski/hamsterclient/MainActivity.java
@@ -2,6 +2,7 @@ package info.rynkowski.hamsterclient;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,6 +13,41 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 
 
