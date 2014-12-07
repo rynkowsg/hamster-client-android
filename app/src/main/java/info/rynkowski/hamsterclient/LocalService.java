9970bd1ec6c7f4fa8b204228edd2f0e62df4d89e
Grzegorz Rynkowski <grzegorz.rynkowski@gmail.com>
2014-12-07 15:41:09 +0100

    Added logs

--- app/src/main/java/info/rynkowski/hamsterclient/LocalService.java
+++ app/src/main/java/info/rynkowski/hamsterclient/LocalService.java
@@ -4,37 +4,45 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
 
 public class LocalService extends Service {
     public class LocalBinder extends Binder {
         LocalService getService() {
+            Log.d(TAG, "LocalBinder.getService()");
             return LocalService.this;
         }
     }
 
     public LocalService() {
+        Log.d(TAG, "LocalService()");
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Log.d(TAG, "onCreate()");
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         super.onStartCommand(intent, flags, startId);
+        Log.d(TAG, "onStartCommand()");
         return Service.START_STICKY;
     }
 
     @Override
     public void onDestroy() {
+        Log.d(TAG, "onDestroy()");
         super.onDestroy();
     }
 
     @Override
     public IBinder onBind(Intent intent) {
+        Log.d(TAG, "onBind()");
         return mBinder;
     }
 
+    private final String TAG = this.getClass().getSimpleName();
     private final IBinder mBinder = new LocalBinder();
 }
--- app/src/main/java/info/rynkowski/hamsterclient/MainActivity.java
+++ app/src/main/java/info/rynkowski/hamsterclient/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -15,6 +16,7 @@ import android.widget.Toast;
 public class MainActivity extends Activity {
     private class LocalServiceConnection implements ServiceConnection {
         public void onServiceConnected(ComponentName className, IBinder service) {
+            Log.d(TAG, "LocalServiceConnection.onServiceConnected()");
             LocalService.LocalBinder binder = (LocalService.LocalBinder) service;
             mBoundService = binder.getService();
 
@@ -23,6 +25,7 @@ public class MainActivity extends Activity {
         }
 
         public void onServiceDisconnected(ComponentName className) {
+            Log.d(TAG, "LocalServiceConnection.onServiceDisconnected()");
             mBoundService = null;
             Toast.makeText(getApplicationContext(),  "LocalService disconnected",
                     Toast.LENGTH_SHORT).show();
@@ -30,22 +33,26 @@ public class MainActivity extends Activity {
     };
 
     public MainActivity() {
+        Log.d(TAG, "MainActivity()");
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate()");
         setContentView(R.layout.activity_main);
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
+        Log.d(TAG, "onRestoreInstanceState()");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        Log.d(TAG, "onStart()");
         doStartService();
         doBindService();
     }
@@ -53,20 +60,24 @@ public class MainActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
+        Log.d(TAG, "onResume()");
     }
 
     @Override
     protected void onPause() {
+        Log.d(TAG, "onPause()");
         super.onPause();
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState()");
     }
 
     @Override
     protected void onStop() {
+        Log.d(TAG, "onStop()");
         doUnbindService();
         doStopService();
         super.onStop();
@@ -74,12 +85,14 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onDestroy() {
+        Log.d(TAG, "onDestroy()");
         super.onDestroy();
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        Log.d(TAG, "onCreateOptionsMenu()");
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
@@ -87,6 +100,7 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        Log.d(TAG, "onOptionsItemSelected()");
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -104,11 +118,13 @@ public class MainActivity extends Activity {
         doBindService(new Intent(this, LocalService.class));
     }
     void doBindService(Intent serviceIntent) {
+        Log.d(TAG, "doBindService()");
         bindService(serviceIntent, mServiceConnection, Context.BIND_AUTO_CREATE);
         mIsServiceBound = true;
     }
 
     void doUnbindService() {
+        Log.d(TAG, "doUnbindService()");
         if (mIsServiceBound) {
             // Detach our existing connection.
             unbindService(mServiceConnection);
@@ -120,6 +136,7 @@ public class MainActivity extends Activity {
         doStartService(new Intent(this, LocalService.class));
     }
     private void doStartService(Intent serviceIntent) {
+        Log.d(TAG, "doStartService()");
         startService(serviceIntent);
     }
 
@@ -127,9 +144,11 @@ public class MainActivity extends Activity {
         doStopService(new Intent(this, LocalService.class));
     }
     private void doStopService(Intent serviceIntent) {
+        Log.d(TAG, "doStopService()");
         stopService(serviceIntent);
     }
 
+    private final String TAG = this.getClass().getSimpleName();
     private LocalService mBoundService = null;
     private LocalServiceConnection mServiceConnection = new LocalServiceConnection();
     private boolean mIsServiceBound;
