ad4525c136a5d7e7244e5c224200157ee92ad556
Grzegorz Rynkowski <grzegorz.rynkowski@gmail.com>
2014-12-20 16:26:04 +0100

    Sliding menu using navigation drawer from tutorial, part 1
    Link:
    http://www.androidhive.info/2013/11/android-sliding-menu-using-navigation-drawer/

--- app/src/main/java/info/rynkowski/hamsterclient/view/MainActivity.java
+++ app/src/main/java/info/rynkowski/hamsterclient/view/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Intent;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -12,13 +13,12 @@ import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import org.freedesktop.dbus.exceptions.DBusException;
+
+import java.util.ArrayList;
 
 import info.rynkowski.hamsterclient.R;
 import info.rynkowski.hamsterclient.service.HamsterService;
@@ -31,9 +31,25 @@ public class MainActivity extends Activity implements InterfaceMainActivity {
     private MainActivityHelper helper;
     private Fragment fragment;
 
-    private DrawerLayout dLayout;
-    private ListView dListView;
-    private ArrayAdapter<String> dAdapter;
+//    private DrawerLayout dLayout;
+//    private ListView dListView;
+//    private ArrayAdapter<String> dAdapter;
+
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+
+    // nav drawer title
+    private CharSequence mDrawerTitle;
+
+    // used to store app title
+    private CharSequence mTitle;
+
+    // slide menu items
+    private String[] navMenuTitles;
+    private TypedArray navMenuIcons;
+
+    private ArrayList<NavDrawerItem> navDrawerItems;
+    private NavDrawerListAdapter adapter;
 
     //----------------  Message handling and sending  --------------------------------------------//
     private class LocalHandler extends Handler {
@@ -78,20 +94,59 @@ public class MainActivity extends Activity implements InterfaceMainActivity {
         this.service = new ServiceManager(MainActivity.this, HamsterService.class, new LocalHandler());
         this.helper = new MainActivityHelper(MainActivity.this);
 
-        final String[] titles = getResources().getStringArray(R.array.nav_drawer_items);
-        dLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        dListView = (ListView) findViewById(R.id.left_drawer);
-        dAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, titles);
-        dListView.setAdapter(dAdapter);
-        dListView.setSelector(android.R.color.holo_blue_dark);
-        dListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> arg0, View v, int position, long id) {
-                dLayout.closeDrawers();
-                Toast.makeText(getApplicationContext(), titles[position] + " picked", Toast.LENGTH_SHORT).show();
-                displayView(FragmentFactory.getType(position));
-            }
-        });
+//        final String[] titles = getResources().getStringArray(R.array.nav_drawer_items);
+//        dLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        dListView = (ListView) findViewById(R.id.left_drawer);
+//        dAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, titles);
+//        dListView.setAdapter(dAdapter);
+//        dListView.setSelector(android.R.color.holo_blue_dark);
+//        dListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> arg0, View v, int position, long id) {
+//                dLayout.closeDrawers();
+//                Toast.makeText(getApplicationContext(), titles[position] + " picked", Toast.LENGTH_SHORT).show();
+//                displayView(FragmentFactory.getType(position));
+//            }
+//        });
+
+        mTitle = mDrawerTitle = getTitle();
+
+        // load slide menu items
+        navMenuTitles = getResources().getStringArray(R.array.nav_drawer_items);
+
+        // nav drawer icons from resources
+        navMenuIcons = getResources().obtainTypedArray(R.array.nav_drawer_icons);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.list_slidermenu);
+
+        navDrawerItems = new ArrayList<NavDrawerItem>();
+
+        // adding nav drawer items to array
+        // Test
+        navDrawerItems.add(new NavDrawerItem(navMenuTitles[0], navMenuIcons.getResourceId(0, -1)));
+        // Home
+        navDrawerItems.add(new NavDrawerItem(navMenuTitles[1], navMenuIcons.getResourceId(1, -1)));
+        // History
+        navDrawerItems.add(new NavDrawerItem(navMenuTitles[2], navMenuIcons.getResourceId(2, -1)));
+        // Stats
+        navDrawerItems.add(new NavDrawerItem(navMenuTitles[3], navMenuIcons.getResourceId(3, -1), true, "22"));
+        // Edit tables
+        navDrawerItems.add(new NavDrawerItem(navMenuTitles[4], navMenuIcons.getResourceId(4, -1)));
+        // About
+        navDrawerItems.add(new NavDrawerItem(navMenuTitles[5], navMenuIcons.getResourceId(5, -1), true, "50+"));
+
+        // Recycle the typed array
+        navMenuIcons.recycle();
+
+        // setting the nav drawer list adapter
+        adapter = new NavDrawerListAdapter(getApplicationContext(), navDrawerItems);
+        mDrawerList.setAdapter(adapter);
+
+        // enabling action bar app icon and behaving it as toggle button
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
 
         if (savedInstanceState == null) {
             // on first time display view for first nav item
