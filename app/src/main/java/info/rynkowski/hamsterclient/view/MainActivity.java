14ef5b7367ce05b8f1b51f12037f57baafd04f3c
Grzegorz Rynkowski <grzegorz.rynkowski@gmail.com>
2014-12-25 00:58:01 +0100

    Removed commented old code

--- app/src/main/java/info/rynkowski/hamsterclient/view/MainActivity.java
+++ app/src/main/java/info/rynkowski/hamsterclient/view/MainActivity.java
@@ -30,22 +30,6 @@ public class MainActivity extends ActionBarActivity implements InterfaceMainActi
     Toolbar toolbar;
     NavigationDrawerFragment drawerFragment;
 
-//    private DrawerLayout mDrawerLayout;
-//    private ListView mDrawerList;
-//    private ActionBarDrawerToggle mDrawerToggle;
-//
-//    // nav drawer title
-//    private CharSequence mDrawerTitle;
-//
-//    // used to store app title
-//    private CharSequence mTitle;
-//
-//    // slide menu items
-//    private String[] navMenuTitles;
-//    private TypedArray navMenuIcons;
-//
-//    private ArrayList<NavDrawerItem> navDrawerItems;
-
     //----------------  Message handling and sending  --------------------------------------------//
     private class LocalHandler extends Handler {
         @Override
@@ -97,69 +81,9 @@ public class MainActivity extends ActionBarActivity implements InterfaceMainActi
         drawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
         drawerFragment.setUp(toolbar);
 
-
-//        mTitle = mDrawerTitle = getTitle();
-//
-//        // load slide menu items
-//        navMenuTitles = getResources().getStringArray(R.array.nav_drawer_items);
-//
-//        // nav drawer icons from resources
-//        navMenuIcons = getResources().obtainTypedArray(R.array.nav_drawer_icons);
-//
-//        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-//        mDrawerList = (ListView) findViewById(R.id.left_drawer);
-//
-//        navDrawerItems = new ArrayList<NavDrawerItem>();
-//
-//        // adding nav drawer items to array
-//        // Test
-//        navDrawerItems.add(new NavDrawerItem(navMenuTitles[0], navMenuIcons.getResourceId(0, -1)));
-//        // Home
-//        navDrawerItems.add(new NavDrawerItem(navMenuTitles[1], navMenuIcons.getResourceId(1, -1)));
-//        // History
-//        navDrawerItems.add(new NavDrawerItem(navMenuTitles[2], navMenuIcons.getResourceId(2, -1)));
-//        // Stats
-//        navDrawerItems.add(new NavDrawerItem(navMenuTitles[3], navMenuIcons.getResourceId(3, -1)));
-//        // Edit tables
-//        navDrawerItems.add(new NavDrawerItem(navMenuTitles[4], navMenuIcons.getResourceId(4, -1)));
-//        // About
-//        navDrawerItems.add(new NavDrawerItem(navMenuTitles[5], navMenuIcons.getResourceId(5, -1)));
-//
-//        // Recycle the typed array
-//        navMenuIcons.recycle();
-//
-//        // Set the adapter for the list view
-//        mDrawerList.setAdapter(new NavDrawerListAdapter(getApplicationContext(), navDrawerItems));
-//
-//        // Set the list's click listener
-//        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
-//
 //        // enabling action bar app icon and behaving it as toggle button
 //        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
 //        getSupportActionBar().setHomeButtonEnabled(true);
-
-//        mDrawerToggle = new ActionBarDrawerToggle(MainActivity.this, mDrawerLayout,
-////                R.drawable.ic_drawer, //nav menu toggle icon
-//                R.string.app_name, // nav drawer open - description for accessibility
-//                R.string.app_name // nav drawer close - description for accessibility
-//        ) {
-//            @Override
-//            public void onDrawerClosed(View drawerView) {
-////                super.onDrawerClosed(drawerView);
-//                getSupportActionBar().setTitle(mTitle);
-//                // calling onPrepareOptionsMenu() to show action bar icons
-//                invalidateOptionsMenu();
-//            }
-//
-//            @Override
-//            public void onDrawerOpened(View drawerView) {
-////                super.onDrawerOpened(drawerView);
-////                getSupportActionBar().setTitle(mDrawerTitle);
-//                // calling onPrepareOptionsMenu() to hide action bar icons
-//                invalidateOptionsMenu();
-//            }
-//        };
-//        mDrawerLayout.setDrawerListener(mDrawerToggle);
 
         if (savedInstanceState == null) {
             // on first time display view for first nav item
