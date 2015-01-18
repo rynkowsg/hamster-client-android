791c890d0cc9465dcbd9f3d847ffa71e417ab695
Grzegorz Rynkowski <grzegorz.rynkowski@gmail.com>
2015-01-18 14:12:20 +0100

    Rewroted AddFactActivity using ButterKnife
    I also renamed ids of components.

--- app/src/main/java/info/rynkowski/hamsterclient/ui/AddFactActivity.java
+++ app/src/main/java/info/rynkowski/hamsterclient/ui/AddFactActivity.java
@@ -4,6 +4,7 @@ import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -23,71 +24,31 @@ import java.util.List;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import butterknife.OnClick;
 import info.rynkowski.hamsterclient.R;
 import info.rynkowski.hamsterclient.model.Fact;
 
 public class AddFactActivity extends BaseActivity {
     private static final String TAG = AddFactActivity.class.getName();
 
-    @InjectView(R.id.editStartTime)
-    EditText mInputStartTime;
-    //EditText mInputStartTime;
-    private EditText mInputEndTime;
-    private EditText mInputActivity;
-    private EditText mInputCategory;
-    private EditText mInputTags;
-    private EditText mInputDescription;
+    @InjectView(R.id.start_time)
+    EditText mStartTime;
+    @InjectView(R.id.end_time)
+    EditText mEndTime;
+    @InjectView(R.id.activity)
+    EditText mActivity;
+    @InjectView(R.id.category)
+    EditText mCategory;
+    @InjectView(R.id.tags)
+    EditText mTags;
+    @InjectView(R.id.description)
+    EditText mDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_fact);
         ButterKnife.inject(this);
-
-//        mInputStartTime = (EditText) findViewById(R.id.editStartTime);
-        mInputEndTime = (EditText) findViewById(R.id.editEndTime);
-        mInputActivity = (EditText) findViewById(R.id.editActivity);
-        mInputCategory = (EditText) findViewById(R.id.editCategory);
-        mInputTags = (EditText) findViewById(R.id.editTags);
-        mInputDescription = (EditText) findViewById(R.id.editDescription);
-
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = getIntent();
-                intent.putExtra("fact", readFactFields());
-                AddFactActivity.this.setResult(RESULT_OK, intent);
-                AddFactActivity.this.finish();
-            }
-        });
-
-        View.OnClickListener listener = new View.OnClickListener() {
-            @Override
-            public void onClick(final View view) {
-                Calendar mcurrentTime = Calendar.getInstance();
-                int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
-                int minute = mcurrentTime.get(Calendar.MINUTE);
-                TimePickerDialog mTimePicker;
-                mTimePicker = new TimePickerDialog(AddFactActivity.this, new TimePickerDialog.OnTimeSetListener() {
-                    @Override
-                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
-                        switch (view.getId()) {
-                            case R.id.editStartTime:
-                                mInputStartTime.setText(selectedHour + ":" + selectedMinute);
-                                break;
-                            case R.id.editDescription:
-                                mInputEndTime.setText(selectedHour + ":" + selectedMinute);
-                                break;
-                        }
-                    }
-                }, hour, minute, true);//Yes 24 hour time
-                mTimePicker.setTitle("Select Time");
-                mTimePicker.show();
-            }
-        };
-        mInputStartTime.setOnClickListener(listener);
-        mInputEndTime.setOnClickListener(listener);
 
         // enabling action bar app icon and behaving it as toggle button
         ActionBar actionBar = getSupportActionBar();
@@ -118,15 +79,46 @@ public class AddFactActivity extends BaseActivity {
         }
     }
 
+    @OnClick({R.id.start_time, R.id.end_time})
+    public void onTimePickerClicked(final View view) {
+        Calendar mcurrentTime = Calendar.getInstance();
+        int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+        int minute = mcurrentTime.get(Calendar.MINUTE);
+        TimePickerDialog mTimePicker;
+        mTimePicker = new TimePickerDialog(AddFactActivity.this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                switch (view.getId()) {
+                    case R.id.start_time:
+                        mStartTime.setText(selectedHour + ":" + selectedMinute);
+                        break;
+                    case R.id.end_time:
+                        mEndTime.setText(selectedHour + ":" + selectedMinute);
+                        break;
+                }
+            }
+        }, hour, minute, true);//Yes 24 hour time
+        mTimePicker.setTitle("Select Time");
+        mTimePicker.show();
+    }
+
+    @OnClick(R.id.ok)
+    public void onButtonClicked(View view) {
+        Intent intent = getIntent();
+        intent.putExtra("fact", readFactFields());
+        AddFactActivity.this.setResult(RESULT_OK, intent);
+        AddFactActivity.this.finish();
+    }
+
     private Fact readFactFields() {
         Fact fact = new Fact();
-        fact.setStartTime(getTimestamp(mInputStartTime.getText().toString()));
-        fact.setEndTime(getTimestamp(mInputEndTime.getText().toString()));
-        fact.setActivity(mInputActivity.getText().toString());
-        fact.setActivity(mInputActivity.getText().toString());
-        fact.setCategory(mInputCategory.getText().toString());
-        fact.setTags(getTags(mInputTags.getText().toString()));
-        fact.setDescription(mInputDescription.getText().toString());
+        fact.setStartTime(getTimestamp(mStartTime.getText().toString()));
+        fact.setEndTime(getTimestamp(mEndTime.getText().toString()));
+        fact.setActivity(mActivity.getText().toString());
+        fact.setActivity(mActivity.getText().toString());
+        fact.setCategory(mCategory.getText().toString());
+        fact.setTags(getTags(mTags.getText().toString()));
+        fact.setDescription(mDescription.getText().toString());
         Log.i(TAG, "fact = " + fact);
         Log.i(TAG, "fact.serialized_name() = " + fact.serialized_name());
         return fact;
